a16f608a003ea66953bf14ba8ff01aed8a84dc51
LucianaLescano <lj.lescano@hotmail.com>
2020-12-12 20:09:23 +0100

    third

--- src/cleaning.py
+++ src/cleaning.py
@@ -5,27 +5,41 @@ import pandas as pd
 
 
 def droping_columns(data, lst):
-    ''' Function to drop columns from a dataset.
-        input = dataframe, list of columns names
-        output = draframe 
+    ''' 
+    Function to drop columns from a dataset.
+    input = dataframe, list of columns names
+    output = draframe 
     '''
     new_data = data.drop(lst, axis=1)
     return new_data
 
 
 def sort_by_elements(data, serie, lst):
-    ''' Function to sort a dataframe by specific values.
-        input = serie, list of values
-        output = dataframe
+    ''' 
+    Function to sort a dataframe by specific values.
+    input = serie, list of values
+    output = dataframe
     '''
     new_data = data[serie.isin(lst)]
     return new_data
 
 
 def new_index(data):
-    ''' Function that resets the index of a dataframe
-        input = dataframe
-        output = dataframe
+    ''' 
+    Function that resets the index of a dataframe
+    input = dataframe
+    output = dataframe
     '''
     new_data = data.reset_index(drop=True)
+    return new_data
+
+
+def re_label(data, dct):
+    '''
+    Function that re-label a column based on a dictionary keys and values.
+    input = dataframe, dictionary
+    output = dataframe
+    '''
+    for k,v in dct.items():
+        new_data = data.replace(k,v)
     return new_data
